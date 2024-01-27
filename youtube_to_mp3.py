f422e5b78e8680ea8a1f50cb01d550e151881e1d
avanishd-3 <avanishdavuluri@gmail.com>
2024-01-27 11:16:24 -0800

    Added converting Youtube videos to audio mp3

---
+++ youtube_to_mp3.py
@@ -0,0 +1,12 @@
+import pytube
+
+
+def youtube_link_to_mp3(link: str) -> None:
+    yt_obj = pytube.YouTube(link)
+
+    audio = yt_obj.streams.filter(only_audio=True).first().download(filename="audio.mp3")
+
+
+
+
+youtube_link_to_mp3("https://youtu.be/SWKR0BzL_z0")
